5b2897409d34c52ad6819656a2a3ab7dcfdaf80d
McArroy <rayhanpradipta2021@gmail.com>
2023-11-30 21:14:49 +0700

    Fixed UI responsive about us page-layout

--- resources/views/pages/aboutus.blade.php
+++ resources/views/pages/aboutus.blade.php
@@ -10,43 +10,48 @@
 
 	@include("includes.navigation_bar")
 
-	<section id="home">
-		<div class="home-left">
-			<img src="/graphics/icons/icon_logo_agora.png" alt="icon_logo_agora">
-		</div>
-		<div class="home-right">
-			<a href="" class="btn"> Siapa kami?</a>
-			<h2 class="home-heading"> Agora </h2>
-			<p class="home-para">Agora Group adalah nama dari kelompok kami, yang merupakan singkatan dari Aplikasi Goresan Anak Muda. Kami adalah sebuah kelompok yang berisi mahasiswa-mahasiswa yang gemar mengembangkan aplikasi-aplikasi dengan menggunakan teknologi VR dan AI. Kami berdiri sejak tahun 2023, dengan misi untuk menciptakan aplikasi-aplikasi yang dapat mengatasi masalah-masalah sosial, lingkungan, dan ekonomi di dunia nyata, serta memberikan hiburan dan edukasi bagi pengguna.</p>
-		</div>
-	</section>
-	<section id="our-Team">
-		<h2>Our Member</h2>
-		<div class="teamContainer">
-			<div class="team-item">
-				<img src="graphics/members/member_not_found.png" alt="members_fadly">
-				<h5 class="member-name">Fadly Kurniawan</h5>
-				<span class="role">J0403221007</span>
+	<section class="aboutus">
+		<div class="contents">
+			<div class="agora">
+				<div class="icon">
+					<img src="/graphics/icons/icon_logo_agora.png" alt="icon_logo_agora">
+				</div>
+				<div class="descriptions">
+					<h5>Siapa Kami?</h5>
+					<h1>Agora</h1>
+					<p>Agora Group adalah nama dari kelompok kami, yang merupakan singkatan dari Aplikasi Goresan Anak Muda. Kami adalah sebuah kelompok yang berisi mahasiswa-mahasiswa yang gemar mengembangkan aplikasi-aplikasi dengan menggunakan teknologi VR dan AI. Kami berdiri sejak tahun 2023, dengan misi untuk menciptakan aplikasi-aplikasi yang dapat mengatasi masalah-masalah sosial, lingkungan, dan ekonomi di dunia nyata, serta memberikan hiburan dan edukasi bagi pengguna.</p>
+				</div>
 			</div>
-			<div class="team-item">
-				<img src="graphics/members/member_not_found.png" alt="members_fauzi">
-				<h5 class="member-name">Fauzi Adi Saputra</h5>
-				<span class="role">J0403221107</span>
-			</div>
-			<div class="team-item">
-				<img src="graphics/members/member_not_found.png" alt="members_fatih">
-				<h5 class="member-name">Fatih Kawakib Kartono</h5>
-				<span class="role">J0403221073</span>
-			</div>
-			<div class="team-item">
-				<img src="graphics/members/member_not_found.png" alt="members_jonser">
-				<h5 class="member-name">Jonser Steven Rajali M.</h5>
-				<span class="role">J0403221163</span>
-			</div>
-			<div class="team-item">
-				<img src="graphics/members/member_not_found.png" alt="members_rayhan">
-				<h5 class="member-name">Rayhan Ananda Hafidz P.</h5>
-				<span class="role">J0403221010</span>
+
+			<div class="members">
+				<h2>Tim Kami</h2>
+				<div class="container">
+					<div class="item" title="Fadly Kurniawan">
+						<img src="/graphics/members/member_fadly.png" alt="member_fadly">
+						<h5>Fadly Kurniawan</h5>
+						<span>J0403221007</span>
+					</div>
+					<div class="item" title="Fauzi Adi Saputra">
+						<img src="/graphics/members/member_fauzi.png" alt="member_fauzi">
+						<h5>Fauzi Adi Saputra</h5>
+						<span>J0403221107</span>
+					</div>
+					<div class="item" title="Fatih Kawakib Kartono">
+						<img src="/graphics/members/member_fatih.png" alt="member_fatih">
+						<h5>Fatih Kawakib Kartono</h5>
+						<span>J0403221073</span>
+					</div>
+					<div class="item" title="Jonser Steven Rajali M.">
+						<img src="/graphics/members/member_jonser.png" alt="member_jonser">
+						<h5>Jonser Steven Rajali M.</h5>
+						<span>J0403221163</span>
+					</div>
+					<div class="item" title="Rayhan Ananda Hafiz P.">
+						<img src="/graphics/members/member_rayhan.png" alt="member_rayhan">
+						<h5>Rayhan Ananda Hafiz P.</h5>
+						<span>J0403221010</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	</section>
